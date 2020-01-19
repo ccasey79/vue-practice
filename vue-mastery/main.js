413698861e17cb9e53872f5d36ea9ff3120b52f2
Christian Casey <christiancasey969@gmail.com>
2020-01-19 17:03:33 +0000

    vue basic procedures

--- vue-mastery/main.js
+++ vue-mastery/main.js
@@ -12,41 +12,18 @@ Vue.component('greeting', {
 var app = new Vue ( {
     el: '#app',
     data: {
-        brand: "Vue Mastery",
-        product: 'Boots',
-        image: 'images/scrat.jpg',
-        inStock: false,
-        onSale: true,
-        details: ["80% cotton", "Jet Black", "Very robust" ],
-        sizes: [ 'Size 8', 'Size 9', 'Size 10', 'Size 11', 'Size 12'],
-        variants: [
-            { variantId: 200,
-              variantName: 'Scrat',
-              variantImage: 'images/scrat.jpg',
-            },
-
-            {
-               variantId: 300,
-               variantName: 'Roadrunner', 
-               variantImage:'images/roadrunner.jpg',
-            }
-        ],
-        cart: 0
+        name: 'Natalie',
+        job: 'Ninja',
+        website: 'http://www.google.com',
+        websiteTag: '<a href="http://www.google.com">Google</a>'
     },
     methods: {
-        addToCart: function() {
-            this.cart += 1
-        },
-        subtractFromCart: function() {
-            this.cart -= 1
-        },
-        updateProduct: function(variantImage) {
-            this.image = variantImage
+        greet: function(time) {
+            return 'Good ' +  time + ' ' + this.name;
         }
+
     },
     computed: {
-        title() {
-            return this.brand + ' ' + this.product 
-        }
+
     }
 });
